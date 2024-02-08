ed5a595b51e0fa914ca3a4351a1ad3995cee104d
Magyar-Hunor Tamas <magyar-hunor.tamas@makers.tech>
2024-02-08 13:09:48 +0000

    Update test

--- lib/person.py
+++ lib/person.py
@@ -3,10 +3,13 @@ from lib.weather import Weather
 class Person():
     def __init__(self, name):
         self.name = name
-        self.weather = Weather()
+        self.weather = None
 
     def go_out(self):
         if self.weather.gimme_weather_status() == 'rainy':
             return "Let's go out with an umbrella!"
         if self.weather.gimme_weather_status() == 'sunny':
             return "Let's go out with sunglasses!"
+        
+    def add_weather(self, weather):
+        self.weather = weather
--- tests/test_person_unit.py
+++ tests/test_person_unit.py
@@ -1,18 +1,19 @@
 from lib.person import *
+from lib.weather import *
+from unittest.mock import Mock
 
-def test_person_instantiates_with_name_and_weather():
+def test_person_instantiates_with_name():
     person = Person('Samantha')
 
     actual_name = person.name
-    actual_weather = type(person.weather)
 
     expected_name = 'Samantha'
-    expected_weather = type(Weather())
     assert actual_name == expected_name
-    assert actual_weather == expected_weather
 
 def test_lets_go_out_when_rainy():
     person = Person('Vijay')
+    weather = Weather()
+    person.add_weather(weather)
 
     actual = person.go_out()
     expected = "Let's go out with an umbrella!"
@@ -21,7 +22,8 @@ def test_lets_go_out_when_rainy():
 
 def test_lets_go_out_when_sunny():
     person = Person('Anamaria')
-
+    weather = Weather()
+    person.add_weather(weather)
     actual = person.go_out()
     expected = "Let's go out with sunglasses!"
 
